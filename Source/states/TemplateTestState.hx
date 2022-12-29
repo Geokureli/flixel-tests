155e91af98e23f761987be8a3b2a7e3516835491
George FunBook <gkurelic@gmail.com>
2022-12-28 18:01:59 -0600

    add template

---
+++ Source/states/TemplateTestState.hx
@@ -0,0 +1,16 @@
+package states;
+
+import flixel.FlxG;
+
+class TemplateTestState extends flixel.FlxState
+{
+    override function create()
+    {
+        super.create();
+    }
+    
+    override function update(elapsed)
+    {
+        super.update(elapsed);
+    }
+}
