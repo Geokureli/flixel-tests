c3db3b327fa16f9e56d81fe1a304d1d5190f15a9
George FunBook <gkurelic@gmail.com>
2023-04-30 12:42:04 -0500

    simplify AnimCallbackTestState

--- Source/states/AnimCallbackTestState.hx
+++ Source/states/AnimCallbackTestState.hx
@@ -7,27 +7,16 @@ class AnimCallbackTestState extends flixel.FlxState
 {
     override function create()
     {
-        var sprite:FlxSprite = new FlxSprite().loadGraphic("assets/images/haxe-anim.png", true, 100, 100);
-        sprite.animation.add("anim", [0,1,2,3,4,5,6,7,6,5,4,3,2,1,0], false);
+        var sprite:FlxSprite = new FlxSprite().makeGraphic(100, 100);
+        sprite.animation.add("anim", [0, 0, 0, 0], 4, false);
         add(sprite);
         
         // insert animation code here
         sprite.animation.finishCallback = function(anim:String)
         {
-            trace(anim);
-            
-            sprite.kill();
-            remove(sprite, true);
-            
             sprite.destroy();
-            sprite = null;
         }
         
         sprite.animation.play('anim');
-    }
-    
-    override function update(elapsed)
-    {
-        super.update(elapsed);
     }
 }
