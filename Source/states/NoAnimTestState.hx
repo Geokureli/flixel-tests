0ddc1eca25d90d0815eddd8da8c56046c7841d9e
George FunBook <gkurelic@gmail.com>
2023-04-30 12:18:51 -0500

    add AnimCallbackTestState

--- Source/states/NoAnimTestState.hx
+++ Source/states/NoAnimTestState.hx
@@ -7,7 +7,7 @@ class NoAnimTestState extends flixel.FlxState
 	override public function create():Void
 	{
 		var sprite = new FlxSprite().loadGraphic("assets/images/haxe-anim.png", true);
-		sprite.animation.add("idle", [for (i in 0... sprite.animation.frames) i]);
+		sprite.animation.add("idle", [for (i in 0... sprite.animation.numFrames) i]);
 		sprite.screenCenter();
 		trace(sprite.animation.curAnim);
 		add(sprite);
