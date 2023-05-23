f7d48e7ab3b63824959ad3ef2b8c31e586495233
George FunBook <gkurelic@gmail.com>
2023-05-23 13:37:49 -0500

    more asset paths tests

--- Source/Main.hx
+++ Source/Main.hx
@@ -19,7 +19,7 @@ class Main extends openfl.display.Sprite
         // addChild(new flixel.FlxGame(0, 0, states.AustinPixelTestState));
         // addChild(new flixel.FlxGame(0, 0, states.ButtonUpTestState));
         // addChild(new flixel.FlxGame(0, 0, states.FlxTextTestState2656));
-        // addChild(new flixel.FlxGame(0, 0, states.AssetPathsTestState));
+        addChild(new flixel.FlxGame(0, 0, states.AssetPathsTestState));
         // addChild(new flixel.FlxGame(0, 0, states.DebugSelectionTestState));
         // addChild(new flixel.FlxGame(0, 0, states.ButtonZoomTestState));
         // addChild(new flixel.FlxGame(0, 0, states.NoAnimTestState));
@@ -46,7 +46,7 @@ class Main extends openfl.display.Sprite
         // addChild(new flixel.FlxGame(0, 0, states.TryCatchTestState));
         // addChild(new flixel.FlxGame(0, 0, states.EaseGraphState));
         // addChild(new flixel.FlxGame(0, 0, states.TweenChainTestState));
-        addChild(new flixel.FlxGame(0, 0, states.FlxWeaponTestState));
+        // addChild(new flixel.FlxGame(0, 0, states.FlxWeaponTestState));
     }
 }
 
--- Source/states/AssetPathsTestState.hx
+++ Source/states/AssetPathsTestState.hx
@@ -1,25 +1,30 @@
 package states;
 
+import flixel.system.FlxAssets;
 import flixel.FlxG;
 import flixel.FlxSprite;
+import flixel.addons.ui.FlxUIGroup;
+
+@:build(flixel.system.FlxAssets.buildFileReferences("assets/", true))
+class AssetPaths {}
 
 // @:build(flixel.system.FlxAssets.buildFileReferences("assets/", true, [".png"]))
 // class AssetPaths_migrate {}
 
-// @:build(flixel.system.FlxAssets.buildFileReferences("assets/", true))
-// class AssetPaths_simple {}
+// @:build(flixel.system.FlxAssets.buildFileReferences("assets/", true, null, null, null, "fullList"))
+// class AssetPaths_fullList {}
 
 // @:build(flixel.system.FlxAssets.buildFileReferences("assets/", true, null, ~/\/test\/|.ase/))
 // class AssetPaths_exclude {}
 
 // @:build(flixel.system.FlxAssets.buildFileReferences("assets/", true, null, null, function (file)
 // {
-// 	if (file.indexOf(".ase") == file.length - 4)
-// 		return null; // exclude .ase files
+// 	if (file.indexOf("lorem_ipsum.txt") != -1)
+// 		return "allFiles"; 
 	
-// 	var spl = file.toLowerCase().split("assets/").join("").split(".");
-// 	spl.pop();
-// 	return spl.join("__").split(" ").join("_").split("/").join("_");
+// 	final spl = file.toLowerCase().split("assets/").join("").split(".");
+// 	final ext = spl.pop();
+// 	return spl.join("__").split(" ").join("_").split("/").join("_") + "__" + ext;
 // }))
 // class AssetPaths_rename {}
 
@@ -37,8 +42,15 @@ class AssetPathsTestState extends flixel.FlxState
 {
 	override public function create():Void
 	{
+		for (path in AssetPaths.allFiles)
+			trace(path);
+		
 		// AssetPaths_migrate.haxe__png;
-		// AssetPaths_simple.
+		// trace(AssetPaths_simple.a__png);
+		
+		// for (path in AssetPaths_fullList.fullList)
+		// 	trace(path);
+		
 		// AssetPaths_exclude.
 		// AssetPaths_rename.images_haxe;
 		
