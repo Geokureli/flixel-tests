62b6c3236503509f72f05160f5517abc05167931
George FunBook <gkurelic@gmail.com>
2023-10-03 12:39:56 -0500

    test new features

--- Source/states/AseAtlasTestState.hx
+++ Source/states/AseAtlasTestState.hx
@@ -1,5 +1,6 @@
 package states;
 
+import flixel.graphics.atlas.AseAtlas;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.graphics.frames.FlxAtlasFrames;
@@ -10,50 +11,74 @@ using flixel.graphics.FlxAsepriteUtil;
 
 class AseAtlasTestState extends flixel.FlxState
 {
-    var sprite:AseAtlasSprite;
-    
     override function create()
     {
         super.create();
         
-        sprite = new AseAtlasSprite("adventurerAssets/adventurer.png", "adventurerAssets/adventurer.json");
-        sprite.animation.play("idle");
-        add(sprite);
-        
-        final a = new flixel.FlxObject();
-        final b = new flixel.FlxObject();
-        
-        FlxG.collide(a, b, function (a:flixel.FlxObject, b:flixel.FlxObject) { });
-        FlxG.watch
-    }
-    
-    override function update(elapsed)
-    {
-        super.update(elapsed);
-        
-        if (FlxG.keys.pressed.RIGHT)
-        {
-            sprite.flipX = false;
-            sprite.animation.play("run");
-        }
-        else if (FlxG.keys.pressed.LEFT)
-        {
-            sprite.flipX = true;
-            sprite.animation.play("run");
-        }
-        else if (FlxG.keys.pressed.DOWN)
-            sprite.animation.play("crouch");
-        else
-            sprite.animation.play("idle");
+        add(new AdventurerSprite(  0, 50, 0.75));
+        add(new AdventurerSprite( 50, 50, 1.00));
+        add(new AdventurerSprite(100, 50, 1.25));
+        add(new AdventurerSprite(150, 50, 1.50));
     }
 }
 
-@:forward
-abstract AseAtlasSprite(FlxSprite) to FlxSprite
+class AdventurerSprite extends AseAtlasSprite
+{
+    static inline var ATTACK = "attack2";
+    
+    public var attackTimeScale:Float;
+    
+    public function new(x = 0, y = 0, attackTimeScale = 1.0)
+    {
+        this.attackTimeScale = attackTimeScale;
+        super(x, y, "adventurerAssets/adventurer.png", "adventurerAssets/adventurer.json");
+        animation.play("idle");
+    }
+    
+    override function update(elapsed:Float)
+    {
+        super.update(elapsed);
+        
+        if (animation.curAnim.name == ATTACK && animation.finished)
+        {
+            animation.play("idle");
+            animation.timeScale = 1.0;
+        }
+        
+        if (animation.curAnim.name != ATTACK)
+        {
+            if (FlxG.keys.pressed.RIGHT)
+            {
+                flipX = false;
+                animation.play("run");
+            }
+            else if (FlxG.keys.pressed.LEFT)
+            {
+               flipX = true;
+               animation.play("run");
+            }
+            else if (FlxG.keys.pressed.DOWN)
+                animation.play("crouch");
+            else
+            {
+                if (FlxG.keys.pressed.SPACE)
+                {
+                    animation.play(ATTACK);
+                    animation.timeScale = attackTimeScale;
+                }
+                else if (animation.curAnim.name != "idle")
+                    animation.play("idle");
+            }
+        }
+    }
+}
+
+class AseAtlasSprite extends FlxSprite
 {
     public function new(x = 0, y = 0, graphic, data:FlxAsepriteJsonAsset)
     {
-        this = new FlxSprite(x, y);
-        this.loadAseAtlasAndTagsByPrefix(graphic, data);
+        super(x, y);
+        
+        FlxAsepriteUtil.loadAseAtlasAndTagsByPrefix(this, graphic, data);
     }
 }
