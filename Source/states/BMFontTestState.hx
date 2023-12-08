574f0d8c24fe7b6d44e5428151460c97c3ba4ebe
George ZenBook <gkurelic@gmail.com>
2023-12-08 08:47:40 -0800

    add arial font

--- Source/states/BMFontTestState.hx
+++ Source/states/BMFontTestState.hx
@@ -12,23 +12,36 @@ class BMFontTestState extends flixel.FlxState
 	{
 		super.create();
 		
-		final texts = new FlxTypedSpriteGroup();
+		// var x = 0.0;
 		var y = 0.0;
-		
-		function createText(fontKey:String, text:String)
+		function createGroup(assetId:String)
 		{
-			// use a unique bitmap data to prevent caching from using a cahced font
-			final graphic = FlxG.bitmap.add('assets/fonts/DigitalPup.png', true);
-			final font = FlxBitmapFont.fromAngelCode(graphic, fontKey);
-			final text = new FlxBitmapText(0, y, text, font);
-			y += text.height + 5;
-			texts.add(text);
+			// var y = 0.0;
+			final texts = new FlxTypedSpriteGroup<FlxBitmapText>();
+			
+			function createText(fontKey:String, text:String)
+			{
+				// use a unique bitmap data to prevent caching from using a cached font
+				final graphic = FlxG.bitmap.add('assets/fonts/$assetId.png', true);
+				final font = FlxBitmapFont.fromAngelCode(graphic, fontKey);
+				final text = new FlxBitmapText(0, y, text, font);
+				y += text.height + 5;
+				trace(text.width);
+				return text;
+			}
+			
+			texts.add(createText('assets/fonts/$assetId.xml', 'This $assetId font uses XML'));
+			texts.add(createText('assets/fonts/$assetId.txt', 'This $assetId font uses Text'));
+			texts.add(createText('assets/fonts/$assetId.fnt', 'This $assetId font uses Binary'));
+			// x += texts.width + 5;
+			
+			return texts;
 		}
 		
-		createText('assets/fonts/BMFont XML/DigitalPup.xml', 'This font uses XML');
-		createText('assets/fonts/BMFont Text/DigitalPup.txt', 'This font uses Text');
-		createText('assets/fonts/BMFont Binary/DigitalPup.fnt', 'This font uses Binary');
-		add(texts);
-		texts.screenCenter();
+		final groups = new FlxTypedSpriteGroup<FlxTypedSpriteGroup<FlxBitmapText>>();
+		groups.add(createGroup("DigitalPup"));
+		groups.add(createGroup("arial"));
+		groups.screenCenter();
+		add(groups);
 	}
 }
