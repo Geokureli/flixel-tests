662452912125eb36ee9ef48a1fe3cb5fd59369ab
George FunBook <gkurelic@gmail.com>
2023-12-08 11:56:37 -0600

    move fonts to fonts, ignore arial

--- Source/states/BMFontTestState.hx
+++ Source/states/BMFontTestState.hx
@@ -11,36 +11,52 @@ class BMFontTestState extends flixel.FlxState
 	override public function create()
 	{
 		super.create();
+	}
+	
+	override function update(elapsed:Float)
+	{
+		super.update(elapsed);
 		
+		if (FlxG.mouse.pressed && length == 0)
+		{
+			createAllTexts();
+		}
+	}
+	
+	public function createAllTexts()
+	{
+		final groups = new FlxTypedSpriteGroup<FlxTypedSpriteGroup<FlxBitmapText>>();
 		// var x = 0.0;
 		var y = 0.0;
 		function createGroup(assetId:String)
 		{
+			if (Assets.exists('fonts/$assetId.png') == false)
+				return;
+			
 			// var y = 0.0;
 			final texts = new FlxTypedSpriteGroup<FlxBitmapText>();
+			groups.add(texts);
 			
 			function createText(fontKey:String, text:String)
 			{
 				// use a unique bitmap data to prevent caching from using a cached font
-				final graphic = FlxG.bitmap.add('assets/fonts/$assetId.png', true);
+				final graphic = FlxG.bitmap.add('fonts/$assetId.png', true);
 				final font = FlxBitmapFont.fromAngelCode(graphic, fontKey);
 				final text = new FlxBitmapText(0, y, text, font);
 				y += text.height + 5;
-				trace(text.width);
-				return text;
+				
+				texts.add(text);
 			}
 			
-			texts.add(createText('assets/fonts/$assetId.xml', 'This $assetId font uses XML'));
-			texts.add(createText('assets/fonts/$assetId.txt', 'This $assetId font uses Text'));
-			texts.add(createText('assets/fonts/$assetId.fnt', 'This $assetId font uses Binary'));
+			createText('fonts/$assetId.xml', 'This $assetId font uses XML');
+			createText('fonts/$assetId.txt', 'This $assetId font uses Text');
+			createText('fonts/$assetId.fnt', 'This $assetId font uses Binary');
+			y += 5;// group spacing
 			// x += texts.width + 5;
-			
-			return texts;
 		}
 		
-		final groups = new FlxTypedSpriteGroup<FlxTypedSpriteGroup<FlxBitmapText>>();
-		groups.add(createGroup("DigitalPup"));
-		groups.add(createGroup("arial"));
+		createGroup("digitalpup");
+		createGroup("arial");
 		groups.screenCenter();
 		add(groups);
 	}
